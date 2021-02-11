49a9427106b95f87a31c11375f9d7abf8496f9e9
Yutaro Hiraoka <yutaro.21.com@gmail.com>
2021-02-11 11:35:26 +0900

    api request local state

--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,150 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        nuxt-axios-request-pattern
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <h1
+      class="title"
+      v-text="'nuxt-axios-request-pattern'"
+    />
+    <h2
+      class="subtitle"
+      v-text="'コメント一覧 ( ローカルステート, CSR )'"
+    />
+    <button
+      type="button"
+      class="button"
+      v-text="'コメント一覧を取得する'"
+      @click="fetchComments"
+    />
+    <ul
+      v-if="comments"
+      class="list"
+    >
+      <li
+        v-for="comment in comments.data"
+        :key="comment.id"
+        class="listItem"
+      >
+        <p v-text="`id: ${comment.id}`" />
+        <p v-text="comment.body" />
+      </li>
+    </ul>
+    <p
+      v-else
+      class="text"
+      v-text="'未取得です'"
+    />
+    <h2
+      class="subtitle"
+      v-text="'カテゴリ一覧 ( ローカルステート, SSR )'"
+    />
+    <ul class="list">
+      <li
+        v-for="categoy in categories.data"
+        :key="categoy.id"
+        class="listItem"
+      >
+        <p v-text="`id: ${categoy.id}`" />
+        <p v-text="categoy.title" />
+        <p v-text="categoy.description" />
+      </li>
+    </ul>
+    <h2
+      class="subtitle"
+      v-text="'投稿一覧 ( ローカルステート, SSR )'"
+    />
+    <ul class="list">
+      <li
+        v-for="post in posts.data"
+        :key="post.id"
+        class="listItem"
+      >
+        <p v-text="`id: ${post.id}`" />
+        <p v-text="post.title" />
+        <p v-text="post.description" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-
-export default Vue.extend({})
+import { Context } from '@nuxt/types'
+interface Data {
+  categories: null | { data: { id: number, title: string, description: string }[] },
+  posts: null | { data: { id: number, title: string, description: string }[] },
+  comments: null | { data: { id: number, body: string }[] },
+}
+export default Vue.extend({
+  async asyncData ({ app }: Context): Promise<Data> {
+    const [categories, posts] = await Promise.all([
+      app.$repositories('categories').index(),
+      app.$repositories('posts').index()
+    ])
+    return {
+      categories: categories.data,
+      posts: posts.data
+    }
+  },
+  data (): Data {
+    return {
+      categories: null,
+      posts: null,
+      comments: null
+    }
+  },
+  methods: {
+    async fetchComments (): void {
+      try {
+        const response = await this.$repositories('comments').index()
+        this.comments = response.data
+      } catch (error) {
+        console.log(error.response)
+        throw error
+      }
+    }
+  }
+})
 </script>
 
 <style>
 .container {
   margin: 0 auto;
+  padding: 30px;
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
 }
-
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
+  font-size: 3rem;
+  font-weight: bold;
   color: #35495e;
-  letter-spacing: 1px;
+  display: block;
 }
-
 .subtitle {
+  font-size: 1.5rem;
   font-weight: 300;
-  font-size: 42px;
   color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+  margin-top: 30px;
 }
-
-.links {
-  padding-top: 15px;
+.list {
+  list-style: none;
+  padding-left: 0;
+  margin-top: 10px;
+  display: flex;
+  flex-wrap: wrap;
+}
+.listItem {
+  border: 1px solid #526488;
+  border-radius: 4px;
+  padding: 10px;
+  width: calc(50% - 5px);
+  min-width: calc(50% - 5px);
+  margin: 10px 0 0 10px;
+}
+.listItem:nth-child(odd) {
+  margin-left: 0;
+}
+.button {
+  margin-top: 10px;
+}
+.text {
+  margin-top: 10px;
 }
 </style>
