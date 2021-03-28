30a105162d69a2e0a615b990d6fdf60c2ad9819d
Yutaro Hiraoka <yutaro.21.com@gmail.com>
2021-03-28 09:53:48 +0900

    update : nuxt.config を ts 化

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,49 +1,40 @@
-export default {
+import { Configuration } from '@nuxt/types'
+const nuxtConfig: Configuration = {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nuxt-axios-request-pattern',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'ja'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'description' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-
+  css: [],
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '@/plugins/api',
     '@/plugins/mock'
   ],
-
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
+  components: false,
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    '@nuxt/typescript-build', // https://go.nuxtjs.dev/typescript
     'nuxt-typed-vuex'
   ],
-
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    '@nuxtjs/axios' // https://go.nuxtjs.dev/axios
   ],
-
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
-
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: [
@@ -51,3 +42,5 @@ export default {
     ]
   }
 }
+
+module.exports = nuxtConfig
